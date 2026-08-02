fd7174223f25db8bc4677c28f2327ce0dee0f121
Monakh <monakh3105@gmail.com>
2026-08-02 11:22:34 +0530

    Adding Docker in backend

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,15 +1,15 @@
 import admin from "firebase-admin";
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
-    }),
-  });
-}
+// if (!admin.apps.length) {
+//   admin.initializeApp({
+//     credential: admin.credential.cert({
+//       projectId: process.env.FIREBASE_PROJECT_ID,
+//       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+//       privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+//     }),
+//   });
+// }
 
-console.log("✅ Firebase Admin initialized via ENV");
+console.log("Failed");
 
 export default admin;
